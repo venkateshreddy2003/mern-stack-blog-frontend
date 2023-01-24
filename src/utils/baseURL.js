7f16d3b8c8d721d3e13dd46725e5ed58baf1c562
venkateshreddy2003 <20bq1a05j4@vvit.net>
2023-01-24 18:36:55 +0530

    commit

---
+++ src/utils/baseURL.js
@@ -0,0 +1,4 @@
+const baseUrlLocal = "https://mern-stack-blog-backend.onrender.com";
+// const baseUrlLocal = "http://localhost:9000";
+// const baseUrl = process.env.REACT_APP_API_URL;
+export default baseUrlLocal;
